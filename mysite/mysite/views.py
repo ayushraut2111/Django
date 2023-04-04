e2ed5e9b8d0fab314b4f806ff736ab3066a7b1ce
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-04 16:56:47 +0530

    importing request and response

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,6 +1,6 @@
 # i have created this file 
 
-from django.http import HttpResponse
+from django.http import HttpResponse    # to import request and response use this
 
 def index(request):     #any function take request and return response 
     return HttpResponse("hello this is homepage")
