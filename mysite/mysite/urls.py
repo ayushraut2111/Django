e58a8075d3622d55d90eccee36622197e18d3e49
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-05 23:25:36 +0530

    remove number from text and show in web page

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -20,9 +20,5 @@ from . import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.index,name='index'),
-    path('removepunc',views.removepunc,name='rempun'),
-    path('capitalizefirst',views.capfirst,name='capfirst'),
-    path('newlineremove',views.newlineremove,name='newlineremove'),
-    path('spaceremove',views.spaceremove,name='spaceremove'),
-    path('charcount',views.charcount,name='charcount'),
+    path('analyze',views.analyze,name='rempun'),
 ]
--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,4 +1,4 @@
-# i have created this file 
+
 
 from django.http import HttpResponse    # to import request and response use this
 from django.shortcuts import render  # to work with templates
@@ -6,18 +6,13 @@ from django.shortcuts import render  # to work with templates
 def index(request):     #any function take request and return response 
     return render(request,"index.html")   # taking third argument as dictionary and using it in our html file
 
-def removepunc(request):
+def analyze(request):
     djinp=request.GET.get('csk','ayush')
-    print(djinp)
-    return HttpResponse("remove punc <button><a href='/'>back</a></button>")  #adding back button in our page
-def capfirst(request):
-    return HttpResponse("capitalize first")
-
-def newlineremove(request):
-    return HttpResponse("remove new line")
-
-def spaceremove(request):
-    return HttpResponse("remove space")
-
-def charcount(request):
-    return HttpResponse("count char")
+    # print(djinp)
+    num=['0','1','2','3','4','5','6','7','8','9']
+    an=""
+    for i in djinp:
+        if i not in num:
+            an+=i
+    duc={'rmp':'after removing number text is','atext':an}
+    return render(request,"analyze.html",duc)
