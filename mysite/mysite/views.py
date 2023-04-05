25f080059450cd22e95fc0c849134bd869fdd771
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-05 22:05:18 +0530

    adding html templates

--- mysite/mysite/views.py
+++ mysite/mysite/views.py
@@ -1,9 +1,10 @@
 # i have created this file 
 
 from django.http import HttpResponse    # to import request and response use this
+from django.shortcuts import render  # to work with templates
 
 def index(request):     #any function take request and return response 
-    return HttpResponse("Home")
+    return render(request,"index.html")   # using tamplate in our django  using render
 
 def removepunc(request):
     return HttpResponse("remove punc <button><a href='/'>back</a></button>")  #adding back button in our page
