14f88ab934f5d59be5672e667fb5cb01e00e2fcc
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-07 15:07:42 +0530

    adding template to app

--- studybud/base/views.py
+++ studybud/base/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.shortcuts import render
+def home(request):
+    return HttpResponse("hello")
 
-# Create your views here.
+def index(request):
+    return render(request,"index.html")
--- studybud/studybud/urls.py
+++ studybud/studybud/urls.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
-from django.urls import path
-from django.http import HttpResponse
-def home(request):
-    return HttpResponse("hello")
+from django.urls import path,include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index',home,name='home'),
+    path('',include('base.urls')),
 ]
