b90a58647f1f3da5f8f73e5dd440ccfff2642526
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-10 21:33:40 +0530

    making login page

--- tutmodels/base/views.py
+++ tutmodels/base/views.py
@@ -6,3 +6,13 @@ def home(request):
     dic={'fr':features}
     return render(request,'index.html',dic)
  
+def register(request):
+    username=request.POST.get("username")
+    email=request.POST.get("email")
+    details=request.POST.get("details")
+    password=request.POST.get("password")
+    password1=request.POST.get("password1")
+    print(username,email,details,password,password1)
+    if password==password1:
+        pass
+    return render(request,'register.html')
