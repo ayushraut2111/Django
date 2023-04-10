aa7e1035d010ffb4167008bf8bd470a5aefec1d6
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-10 22:23:09 +0530

    authentication form in django

--- tutmodels/base/views.py
+++ tutmodels/base/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth.models import User,auth
+from django.contrib import messages
 from .models import feature
 def home(request):
     features=feature.objects.all()
@@ -6,13 +8,27 @@ def home(request):
     dic={'fr':features}
     return render(request,'index.html',dic)
  
-def register(request):
-    username=request.POST.get("username")
-    email=request.POST.get("email")
-    details=request.POST.get("details")
-    password=request.POST.get("password")
-    password1=request.POST.get("password1")
-    print(username,email,details,password,password1)
-    if password==password1:
-        pass
-    return render(request,'register.html')
+def register(request):   #authentication form
+    if request.method=="POST":
+        username=request.POST.get("username")
+        email=request.POST.get("email")
+        details=request.POST.get("details")
+        password=request.POST.get("password")
+        password1=request.POST.get("password1")
+        print(username,email,details,password,password1)
+        if password==password1:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,"email already exist")
+                return redirect('register')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'username matches')
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=username,email=email,password=password)
+                messages.info(request,'user created')
+                return redirect('login')
+        else:
+            messages.info(request,'please enter the same password')
+            return redirect('register')
+    else:
+        return render(request,'register.html')
