279d5e9a02cf345ebd1af55b8bae53ac61295dae
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-13 00:14:13 +0530

    registration and authentication form

--- registrationform/base/urls.py
+++ registrationform/base/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('',views.index,name="index"),
     path('login',views.login,name="login"),
     path('register',views.register,name="register"),
+    path('logout',views.logout,name="logout"),
 ]
--- registrationform/base/views.py
+++ registrationform/base/views.py
@@ -1,16 +1,48 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.contrib import messages
+from django.contrib.auth.models import User,auth
 def index(request):
     return render(request,"index.html")
 
 def register(reuqest):
     if reuqest.method=="POST":
-        pass
+        usr=reuqest.POST.get("username")
+        eml=reuqest.POST.get("email")
+        pwd1=reuqest.POST.get("password")
+        pwd2=reuqest.POST.get("password1")
+        print(usr,eml,pwd1,pwd2)
+        if pwd1==pwd2:
+            if User.objects.filter(email=eml).exists():
+                messages.info(reuqest,"email alerady exist")
+                return redirect('register')
+            elif User.objects.filter(username=usr).exists():
+                messages.info(reuqest,"username not available")
+                return redirect('register')
+            else:
+                user=User.objects.create_user(username=usr,email=eml,password=pwd1)
+                user.save()
+                messages.info(reuqest,"user created")
+                return redirect('login')
+        else:
+            messages.info(reuqest,"password unmatches")
+            return redirect('register')
     else:    
         return render(reuqest,"register.html")
 
 def login(request):
-    pass
+    if request.method=="POST":
+        usr=request.POST.get("username")
+        pwd=request.POST.get("password")
+        user=auth.authenticate(username=usr,password=pwd)
+        if user is not None:
+            auth.login(request,user)
+            return redirect('/')
+        else:
+            messages.info(request,"user not found")
+            return redirect("login")
+    else:    
+        return render(request,"login.html")
 
 def logout(request):
-    pass
+    auth.logout(request)
+    return redirect("/")
