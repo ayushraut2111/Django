79c4a4835caa2cd11c4890bf1db40d4d70d5826d
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-15 00:32:53 +0530

    final commit to weather app

--- weatherdetector/weather/views.py
+++ weatherdetector/weather/views.py
@@ -1,4 +1,25 @@
 from django.shortcuts import render
-
+import requests, json
 def index(request):
-    return render(request,"index.html")
+    if request.method=='POST':
+        city=request.POST["city"]
+        # print(city)
+        BASE_URL = "https://api.openweathermap.org/data/2.5/weather?"
+
+        URL = BASE_URL + "q=" + city + "&appid=" + "1b3a3a907f567fe52ae4799136331b48"
+        response = requests.get(URL)
+        dt = response.json()
+        # print(type(data))
+        data={
+            'city':city,
+            'country':str(dt['sys']['country']),
+            'coordinate':str(dt['coord']['lon'])+ " "+str(dt['coord']['lat']),
+            'temp':str(dt['main']['temp'])+' k',
+            'pressure':str(dt['main']['pressure']),
+            'humidity':str(dt['main']['humidity'])
+        }
+        # print(data)
+
+    else:
+        data={}
+    return render(request,"index.html",data)
