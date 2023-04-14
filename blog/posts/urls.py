09141d6c76de6eb4f9ade5f3e866e307a6619dd3
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-14 15:06:58 +0530

    final commit of blogging site

--- blog/posts/urls.py
+++ blog/posts/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('',views.index,name="index")
+    path('',views.index,name="index"),
+    path('post/<str:pk>',views.post,name='post')
 ]
--- blog/posts/views.py
+++ blog/posts/views.py
@@ -5,3 +5,7 @@ def index(request):
     pst=Posts.objects.all()
     dic={'posts':pst}
     return render(request,"index.html",dic)
+
+def post(request,pk):
+    pst=Posts.objects.get(id=pk)
+    return render(request,'post.html',{'post':pst})
