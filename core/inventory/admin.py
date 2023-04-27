0093262d74ff6f7e49987cdadc2caa7e76527cb7
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-04-27 15:24:05 +0530

    verbose name

--- core/inventory/admin.py
+++ core/inventory/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Product,Brand
+
+admin.site.register(Product)
+admin.site.register(Brand)
--- core/inventory/models.py
+++ core/inventory/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name=models.CharField("fruit name",max_length=1000)
+    qty=models.IntegerField()
+
+    def __str__(self) -> str:
+        return self.name
+
+class Brand(models.Model):
+    name=models.CharField(max_length=1000)
+
+    def __str__(self):
+        return self.name
