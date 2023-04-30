81dd4fcc2e918596c8fb07ff52009bc58f36f634
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-05-01 00:12:50 +0530

    one to one

--- core/inventory/admin.py
+++ core/inventory/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
-from .models import Product,Brand,student,course
+from .models import Product,Brand
 
 admin.site.register(Product)
 admin.site.register(Brand)
-admin.site.register(student)
-admin.site.register(course)
+# admin.site.register(student)
+# admin.site.register(course)
+# admin.site.register(Room)
+# admin.site.register(studentONE)
