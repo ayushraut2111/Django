115a745befd0319ca320df554d7ade41c006c4b5
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-05-01 17:08:42 +0530

    one to one relation

--- dbqueries/queries/models.py
+++ dbqueries/queries/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    c_id=models.IntegerField(primary_key=True)
+    c_name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.c_name
+    
+class Room(models.Model):
+    r_id=models.IntegerField(primary_key=True)
+    r_name=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.r_name
+    
+class Student(models.Model):
+    roll_no=models.IntegerField(primary_key=True)
+    name=models.CharField(max_length=100)
+    course=models.ForeignKey(Course,on_delete=models.CASCADE)
+    room=models.OneToOneField(Room,on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
