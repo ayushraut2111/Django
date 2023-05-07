0eb5f67a40964c175166eaeead1fcb03de5c14c3
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-05-07 21:23:06 +0530

    jsonresponse

--- gs1/api/views.py
+++ gs1/api/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,JsonResponse
 from .models import Student
 from .serialisers import StudentSerialiser
 from rest_framework.renderers import JSONRenderer
@@ -10,5 +10,6 @@ def home(request):
 def student_details(request):
     stu=Student.objects.all()
     serialise=StudentSerialiser(stu,many=True)
-    json_data=JSONRenderer().render(serialise.data)
-    return HttpResponse(json_data,content_type='application/json')
+    # json_data=JSONRenderer().render(serialise.data)
+    # return HttpResponse(json_data,content_type='application/json')
+    return JsonResponse(serialise.data,safe=False)
