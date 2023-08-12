8614f930ebbbe8c55c749371231d621673355793
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-12 13:32:38 +0530

    using mysql as a dd

---
+++ MySqlDB/sql/serializers.py
@@ -0,0 +1,6 @@
+from rest_framework.serializers import ModelSerializer
+from .models import student
+class StudentSerializer(ModelSerializer):
+    class Meta:
+        model=student
+        fields='__all__'
---
+++ MySqlDB/sql/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+
+
+
+# changes are in setting.py
