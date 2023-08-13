565940ce9b0d0bb3629e14fc9b5254a100b723c4
Ayushgit config --global user.name Ayushgit config --global user.name Ayush <ayushraut2111@gmail.com>
2023-08-13 17:14:44 +0530

    filtering

--- filtering/api/views.py
+++ filtering/api/views.py
@@ -1,14 +1,14 @@
+from rest_framework.generics import ListAPIView,CreateAPIView
 from .models import student
 from .serializers import StudentSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.viewsets import ModelViewSet
 
 
-class studentlist(ModelViewSet):
+class studentlist(ListAPIView,CreateAPIView):
     queryset=student.objects.all()
     serializer_class=StudentSerializer
     filter_backends=[DjangoFilterBackend]
-    filterset_fields=['subject','passby']
+    filterset_fields=['subject','user']
 
     # def get_queryset(self):
     #     return student.objects.filter(passby=self.request.user)
